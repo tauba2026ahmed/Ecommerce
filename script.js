3ce8694f77f51b6b3a1c89ddd2b4bb72c685d37e
Ahmed Abdelhalem <mynewphone200124@gmail.com>
2026-02-13 23:45:48 +0200

    finish navbar and hero section in index.html and responsive

---
+++ script.js
@@ -0,0 +1,38 @@
+const navbar = document.getElementById("navbar");
+const menu = document.getElementById("menu");
+const close = document.getElementById("close")
+
+menu.addEventListener("click", () => {
+    navbar.classList.add("active")
+});
+
+close.addEventListener("click", () => {
+    navbar.classList.remove("active")
+});
+
+// const navbarEle = document.querySelectorAll("#navbar a");
+
+// navbarEle.forEach(link => {
+//     link.addEventListener("click", () => {
+
+//         // نشيل active من الكل
+//         navbarEle.forEach(item => {
+//             item.classList.remove("active");
+//         });
+
+//         // نضيفها للعنصر اللي اتضغط عليه
+//         link.classList.add("active");
+//     });
+// });
+
+const navbarEle = document.querySelectorAll("#navbar a");
+
+const currentPage = window.location.pathname.split("/");
+
+console.log(currentPage)
+
+navbarEle.forEach((link) => {
+    if (link.getAttribute("href") === currentPage) {
+        link.classList.add("active");
+    }
+} )
